90937e92c7324af5c92344a0200f485cb74d419c
Bruno Bernardo <bigsby@live.com>
2017-02-03 15:43:34 +0000

    Add F# Hello

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ var data = {
             id: "hello",
             name: "Hello World!",
             description: "Of course, the first project is a 'Hello, World'.",
-            languages: ["js", "cpp", "py"]
+            languages: ["js", "cpp", "py", "fs"]
         },
         {
             id: "loop",
