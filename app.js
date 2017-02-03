8b830c36f37453eb9551c1a028ca20d30af7d367
Bruno Bernardo <bigsby@live.com>
2017-02-03 15:37:25 +0000

    Add Python Hello

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ var data = {
             id: "hello",
             name: "Hello World!",
             description: "Of course, the first project is a 'Hello, World'.",
-            languages: ["js", "cpp"]
+            languages: ["js", "cpp", "py"]
         },
         {
             id: "loop",
