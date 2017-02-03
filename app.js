6df094f57114fb5c1e0081c665ef11083a1d1113
Bigsby <bigsby@live.com>
2017-02-03 13:25:21 +0000

    Add Runtimes and IDEs

--- app.js
+++ app.js
@@ -4,42 +4,63 @@ var data = {
             id: "js",
             name: "JavaScript",
             link: "https://www.javascript.com/",
+            runtimes: ["node"],
+            ides: ["vscode"],
             current: true
         },
         {
             id: "cpp",
             name: "C++",
             link: "http://www.cplusplus.com/",
+            runtimes: ["native"],
+            ides: ["vscode", "vs"],
             current: true
         },
         {
             id: "py",
             name: "Python",
             link: "https://www.python.org/",
+            runtimes: ["python"],
+            ides: ["vscode"],
             current: true
         },
         {
             id: "fs",
             name: "F#",
             link: "http://fsharp.org/",
+            runtimes: ["net"],
+            ides: ["vscode", "vs"],
             current: true
         },
         {
             id: "go",
             name: "Go",
             link: "https://golang.org/",
+            ides: ["vscode", "eclipse"],
             current: true
         },
         {
             id: "rb",
             name: "Ruby",
             link: "https://www.ruby-lang.org/",
+            runtimes: ["ruby"],
+            ides: ["vscode", "rubyMine"],
             current: true
         },
         {
             id: "cs",
             name: "C#",
             link: "https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx",
+            runtimes: ["net"],
+            ides: ["vscode", "vs"],
+            current: true
+        },
+        {
+            id: "java",
+            name: "Java",
+            link: "https://www.java.com/",
+            runtimes: ["java"],
+            ides: ["vscode", "eclipse", "intelliJ", "netbeans"],
             current: true
         },
         {
@@ -66,7 +87,7 @@ var data = {
             id: "hello",
             name: "Hello World!",
             description: "Of course, the first project is a 'Hello, World'.",
-            langs:[]
+            langs: []
         },
         {
             id: "loop",
@@ -80,12 +101,100 @@ var data = {
             description: "Repeat for every item in a sequence.",
             langs: []
         }
+    ],
+    runtimes: [
+        {
+            id: "node",
+            name: "NodeJS",
+            link: "https://nodejs.org/"
+        },
+        {
+            id: "native",
+            name: "Native"
+        },
+        {
+            id: "python",
+            name: "Python",
+            link: "https://www.python.org/downloads/"
+        },
+        {
+            id: "net",
+            name: ".Net",
+            link: "https://www.microsoft.com/net"
+        },
+        {
+            id: "ruby",
+            name: "Ruby",
+            link: "https://www.ruby-lang.org/en/downloads/"
+        },
+        {
+            id: "java",
+            name: "Java SE",
+            link: "https://java.com/en/download/"
+        }
+    ],
+    ides: [
+        {
+            id: "vs",
+            name: "Visual Studio",
+            link: "https://www.visualstudio.com/"
+        },
+        {
+            id: "vscode",
+            name: "Visual Studio Code",
+            link: "https://code.visualstudio.com/"
+        },
+        {
+            id: "eclipse",
+            name: "Eclipse",
+            link: "http://www.eclipse.org/"
+        },
+        {
+            id: "rubyMine",
+            name: "RubyMine",
+            link: "https://www.jetbrains.com/ruby/"
+        },
+        {
+            id: "intelliJ",
+            name: "InteliiJ IDEA",
+            link: "https://www.jetbrains.com/idea/"
+        },
+        {
+            id: "netbeans",
+            name: "NetBeans",
+            link: "https://netbeans.org/"
+        }
     ]
 };
 
 (function () {
     var appName = "hLangs";
     var templatesRoot = "templates/";
+
+    (function () {
+        data.runtimes.forEach(function (rt) { rt.languages = []; });
+        data.ides.forEach(function (ide) { ide.languages = []; });
+
+        function FindAndReplace(target, prop, list) {
+            var result = [];
+            var source = target[prop];
+
+            if (source) {
+                source.forEach(function (id) {
+                    var item = list.find(function (item) { return item.id === id });
+                    if (item) {
+                        item.languages.push(target);
+                        result.push(item);
+                    }
+                });
+                target[prop] = result;
+            }
+        }
+        data.languages.forEach(function (lang) {
+            FindAndReplace(lang, "runtimes", data.runtimes);
+            FindAndReplace(lang, "ides", data.ides);
+        });
+    })();
 
     var app = angular.module(appName, ["ngSanitize", "ui.router"]);
 
