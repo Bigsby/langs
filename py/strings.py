c6ca8f10c9036608da6d065a796bec02ec833829
Bigsby <bigsby@live.com>
2017-02-10 00:42:45 +0000

    Add py strings

---
+++ py/strings.py
@@ -0,0 +1,3 @@
+print("Joining " + "strings.");
+value = 3;
+print(f"The value {value} in interpolated");
---
+++ py/strings_interpolation.py
@@ -0,0 +1,5 @@
+print("The value %s in interpolated" % value);
+print("The value %(value)s in interpolated" % locals());
+
+print("The value {0} in interpolated".format(value));
+print("The value {value!s} in interpolated".format(**locals()));
