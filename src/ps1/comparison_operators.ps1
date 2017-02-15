2b79911a7b8cd247ef2ce036b08a1d4d770c53a4
Bigsby <bigsby@live.com>
2017-02-15 18:47:06 +0000

    Complete ps1

---
+++ src/ps1/comparison_operators.ps1
@@ -0,0 +1,17 @@
+$result = 1 -eq 2
+Write-Host "1 equals 2 is $result"
+
+$result = 1 -ne 2
+Write-Host "1 not equals 2 is $result"
+
+$result = 1 -gt 2
+Write-Host "1 larger than 2 is $result"
+
+$result = 1 -ge 2
+Write-Host "1 larger than or equals 2 is $result"
+
+$result = 1 -lt 2
+Write-Host "1 less than 2 is $result"
+
+$result = 1 -le 2
+Write-Host "1 less than or equals 2 is $result"
