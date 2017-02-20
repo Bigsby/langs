06275081000c1c1c583d9f3b3676dbfc778d3cb9
Bigsby <bigsby@live.com>
2017-02-20 16:52:31 +0000

    Add local NodeJS server

---
+++ config.js
@@ -0,0 +1,26 @@
+exports.defaultMimeType = "application/octet-stream";
+exports.mimeTypes = {
+    "text/html": [
+        ".html",
+        ".htm"
+    ],
+    "text/javascript": [
+        ".js"
+    ],
+    "application/json": [
+        ".json"
+    ],
+    "text/css": [
+        ".css"
+    ],
+    "image/png": [
+        ".png"
+    ],
+    "image/jpg": [
+        ".jpg",
+        ".jpeg"
+    ]
+};
+
+exports.root = "./docs";
+exports.defaultDocument = "index.html";
