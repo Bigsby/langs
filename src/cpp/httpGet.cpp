be317079f5473257d46f1ded11ed72f9f63dbac8
Bigsby <bigsby@live.com>
2017-03-15 23:18:07 +0000

    Clean up C++ HTTP Get

--- src/cpp/httpGet.cpp
+++ src/cpp/httpGet.cpp
@@ -1,146 +1,86 @@
 #include <windows.h>
 #include <string>
-#include <stdio.h>
 #include <iostream>
 
 using std::string;
+
+using namespace std;
 
 #pragma comment(lib, "ws2_32.lib")
 
 WSADATA wsaData;
 
-
-SOCKET connectToServer(char *szServerName, WORD portNum)
-{
-    struct hostent *hp;
-    unsigned int addr;
-    struct sockaddr_in server;
-    SOCKET conn;
-
-    conn = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (conn == INVALID_SOCKET)
-        return NULL;
-
-    if (inet_addr(szServerName) == INADDR_NONE)
-    {
-        hp = gethostbyname(szServerName);
-    }
-    else
-    {
-        addr = inet_addr(szServerName);
-        hp = gethostbyaddr((char *)&addr, sizeof(addr), AF_INET);
-    }
-
-    if (hp == NULL)
-    {
-        closesocket(conn);
-        return NULL;
-    }
-
-    server.sin_addr.s_addr = *((unsigned long *)hp->h_addr);
-    server.sin_family = AF_INET;
-    server.sin_port = htons(portNum);
-    if (connect(conn, (struct sockaddr *)&server, sizeof(server)))
-    {
-        closesocket(conn);
-        return NULL;
-    }
-    return conn;
-}
-
-int getHeaderLength(char *content)
-{
-    const char *srchStr1 = "\r\n\r\n", *srchStr2 = "\n\r\n\r";
-    char *findPos;
-    int ofset = -1;
-
-    findPos = strstr(content, srchStr1);
-    if (findPos != NULL)
-    {
-        ofset = findPos - content;
-        ofset += strlen(srchStr1);
-    }
-
-    else
-    {
-        findPos = strstr(content, srchStr2);
-        if (findPos != NULL)
-        {
-            ofset = findPos - content;
-            ofset += strlen(srchStr2);
-        }
-    }
-    return ofset;
-}
-
-char *readUrl2(long &bytesReturnedOut, char **headerOut)
-{
-    const int bufSize = 512;
-    char readBuffer[bufSize], sendBuffer[bufSize], tmpBuffer[bufSize];
-    char *tmpResult = NULL, *result;
-    SOCKET conn;
-    string server = "langs.bigsbyspot.org", filepath = "/files/webcall.txt";
-    long totalBytesRead, thisReadSize, headerLen;
-
-    conn = connectToServer((char *)server.c_str(), 80);
-
-    sprintf(tmpBuffer, "GET %s HTTP/1.0", filepath.c_str());
-    strcpy(sendBuffer, tmpBuffer);
-    strcat(sendBuffer, "\r\n");
-    sprintf(tmpBuffer, "Host: %s", server.c_str());
-    strcat(sendBuffer, tmpBuffer);
-    strcat(sendBuffer, "\r\n");
-    strcat(sendBuffer, "\r\n");
-    send(conn, sendBuffer, strlen(sendBuffer), 0);
-
-    totalBytesRead = 0;
-    while (1)
-    {
-        memset(readBuffer, 0, bufSize);
-        thisReadSize = recv(conn, readBuffer, bufSize, 0);
-
-        if (thisReadSize <= 0)
-            break;
-
-        tmpResult = (char *)realloc(tmpResult, thisReadSize + totalBytesRead);
-
-        memcpy(tmpResult + totalBytesRead, readBuffer, thisReadSize);
-        totalBytesRead += thisReadSize;
-    }
-
-    headerLen = getHeaderLength(tmpResult);
-    long contenLen = totalBytesRead - headerLen;
-    result = new char[contenLen + 1];
-    memcpy(result, tmpResult + headerLen, contenLen);
-    result[contenLen] = 0x0;
-    char *myTmp;
-
-    myTmp = new char[headerLen + 1];
-    strncpy(myTmp, tmpResult, headerLen);
-    myTmp[headerLen] = NULL;
-    delete (tmpResult);
-    *headerOut = myTmp;
-
-    bytesReturnedOut = contenLen;
-    closesocket(conn);
-    return (result);
-}
-
 int main()
 {
-    const int bufLen = 1024;
-    long fileSize;
-    char *memBuffer, *headerBuffer;
-    FILE *fp;
+  if (WSAStartup(0x101, &wsaData) != 0)
+  {
+    cout << "Unable to open Winsock library.";
+    return WSAGetLastError();
+  }
 
-    memBuffer = headerBuffer = NULL;
+  SOCKET conn = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (conn == INVALID_SOCKET)
+  {
+    cout << "Unable to create socket.";
+    return -1;
+  }
 
-    if (WSAStartup(0x101, &wsaData) != 0)
-        return -1;
+  struct hostent *hp = gethostbyname("langs.bigsbyspot.org");
+  if (hp == NULL)
+  {
+    closesocket(conn);
+    cout << "Unable to result domain.";
+    return -1;
+  }
 
-    memBuffer = readUrl2(fileSize, &headerBuffer);
-    std::cout << memBuffer;
+  struct sockaddr_in internetAddress;
+  internetAddress.sin_addr.s_addr = *((unsigned long *)hp->h_addr);
+  internetAddress.sin_family = AF_INET;
+  internetAddress.sin_port = htons(80);
 
-    WSACleanup();
-    return 0;
+  if (connect(conn, (struct sockaddr *)&internetAddress, sizeof(internetAddress)))
+  {
+    closesocket(conn);
+    cout << "Unable to open socket connection.";
+    return -1;
+  }
+
+  string httpRequest = "GET /files/webcall.txt\r\nHost: langs.bigsbyspot.org\r\n\r\n";
+  send(conn, httpRequest.c_str(), httpRequest.size(), 0);
+
+  const int bufSize = 512;
+  char readBuffer[bufSize];
+  char *httpResponse = NULL;
+  long totalBytesRead = 0, thisReadSize;
+
+  while (1)
+  {
+    memset(readBuffer, 0, bufSize);
+    thisReadSize = recv(conn, readBuffer, bufSize, 0);
+
+    if (thisReadSize <= 0)
+      break;
+
+    httpResponse = (char *)realloc(httpResponse, thisReadSize + totalBytesRead);
+
+    memcpy(httpResponse + totalBytesRead, readBuffer, thisReadSize);
+    totalBytesRead += thisReadSize;
+  }
+
+  const char *contentSeperator = "\r\n\r\n";
+  char *headersEnd = strstr(httpResponse, contentSeperator);
+  int headerLength = headersEnd - httpResponse;
+  headerLength += strlen(contentSeperator);
+
+  long contentLength = totalBytesRead - headerLength;
+  char *content = new char[contentLength + 1];
+  memcpy(content, httpResponse + headerLength, contentLength);
+  content[contentLength] = 0x0;
+
+  closesocket(conn);
+  WSACleanup();
+
+  std::cout << content;
+
+  return 0;
 }
