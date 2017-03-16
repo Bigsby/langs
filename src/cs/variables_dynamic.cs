47371f9210d966ce9825e57232c0a261e4915f40
Bruno Bernardo <bigsby@live.com>
2017-03-16 11:15:49 +0000

    Add dynamic variables in C# and Java

---
+++ src/cs/variables_dynamic.cs
@@ -0,0 +1,13 @@
+class Program
+{
+	public static void Main()
+	{
+        object sentence = 2;
+		sentence = "This is the sentence.";
+		System.Console.WriteLine(sentence);
+		System.Console.WriteLine(sentence);
+		
+		sentence = "This is another sentence.";
+		System.Console.Write(sentence);
+	}
+}
---
+++ src/java/variables_dynamic.java
@@ -0,0 +1,13 @@
+public class variables_dynamic {
+
+    public static void main(String[] args) {
+		Object sentence = 2;
+        sentence = "This is the sentence.";
+        System.out.println(sentence);
+		System.out.println(sentence);
+		
+		sentence = "This is another sentence.";
+        System.out.println(sentence);
+    }
+
+}
