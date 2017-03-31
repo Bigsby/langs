61d1a33ad2416c5909f2f37d060dcff981c5c4b3
Bruno Bernardo <bigsby@live.com>
2017-03-31 12:26:51 +0100

    Use base components

--- docs/scripts/app/appBuilder.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    ProcessData: require("app/data.js"),
-    RegisterComponents: require("app/components.js"),
-    RegisterStates: require("app/states.js")
-}
---
+++ docs/scripts/appBuilder.js
@@ -0,0 +1,5 @@
+module.exports = {
+    ProcessData: require("data.js"),
+    RegisterComponents: require("components.js"),
+    RegisterStates: require("states.js")
+}
