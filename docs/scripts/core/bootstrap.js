b0f9be2900887df6f4477babb5f5eed4f6ecfde2
Bigsby <bigsby@live.com>
2017-03-31 02:08:59 +0100

    Update bootstrapping

--- docs/scripts/core/bootstrap.js
+++ docs/scripts/core/bootstrap.js
@@ -5,34 +5,62 @@
         "map": {
             "json": "https://cdnjs.cloudflare.com/ajax/libs/systemjs-plugin-json/0.3.0/json.min.js",
         },
+        "meta": {
+            "*.json": {
+                "loader": "json"
+            }
+        },
         "paths": {
             "data": "../../../data"
         }
     });
 
-    SystemJS.import("core/config.json!json").then(function (coreConfig) {
-        SystemJS.import("app/config.json!json").then(function (appConfig) {
-            SystemJS.config(coreConfig.config);
-            SystemJS.config(appConfig.config);
+    SystemJS.registerDynamic("bootstrap", [
+        "core/config.json",
+        "app/config.json"
+    ], true, function (require, exports, module) {
+        const coreConfig = require("core/config.json");
+        const appConfig = require("app/config.json");
 
-            if (appConfig.styles)
-                appConfig.styles.forEach(function (css) {
-                    SystemJS.import(css);
-                });
-
-            const dataRequired = appConfig.initialData && appConfig.initialData.length;
-            var data = {};
-
-            if (appConfig.initialData)
-                appConfig.initialData.forEach(function (file) {
-                    SystemJS.import("data/" + file + ".json!json").then(function (dataContent) {
-                        data[file] = dataContent;
-                    });
-                });
-
-            SystemJS.register("data", [], function () {
-                return data;
+        if (appConfig.styles)
+            appConfig.styles.forEach(function (css) {
+                SystemJS.import(css);
             });
+
+        var appBuilderDeps = coreConfig.config.meta.appBuilder.deps;
+
+        if (appConfig.useRouting){
+            appBuilderDeps.push("angular-ui-router");
+            coreConfig.angular.modules.push("ui.router");
+            coreConfig.angular.configComponents.push("$stateProvider");
+            coreConfig.angular.configComponents.push("$urlRouterProvider");
+        }
+        if (appConfig.ga)
+            appBuilderDeps.push("ga");
+
+        if (appConfig.appBuilderDeps && appConfig.appBuilderDeps.length)
+            appBuilderDeps = appBuilderDeps.concat(appConfig.appBuilderDeps);
+
+        const builderInvokerDeps = coreConfig.builderInvokerDeps;
+        if (appConfig.initialData)
+            appConfig.initialData.forEach(function (file) {
+                const map = "data-" + file;
+                coreConfig.config.map[map] = "data/" + file + ".json";
+                builderInvokerDeps.push(map);
+            });
+
+        SystemJS.config(coreConfig.config);
+        SystemJS.config(appConfig.config);
+
+        SystemJS.registerDynamic("builderInvoker", builderInvokerDeps, true, function (require, exports, module) {
+            const data = {};
+
+            if (appConfig.initialData && appConfig.initialData.length) {
+                appConfig.initialData.forEach(function (file) {
+                    var content = require("data-" + file);
+                    data[file] = content;
+                });
+            }
 
             SystemJS.import("appBuilder").then(function (appBuilder) {
                 const app = angular.module(coreConfig.angularAppName, coreConfig.angular.modules);
@@ -65,11 +93,12 @@
                         $urlRouterProvider.otherwise("/");
                 }]));
 
-
                 angular.bootstrap(document, [coreConfig.angularAppName]);
             });
-
         });
+
+        SystemJS.import("builderInvoker");
     });
 
+    SystemJS.import("bootstrap");
 })();
