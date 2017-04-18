10bbc1af029e27b24ae35ac22ca9664ff538e615
Bigsby <bigsby@live.com>
2017-04-18 23:00:52 +0100

    Corret TypeScript

--- src/ts/comparison_operators.ts
+++ src/ts/comparison_operators.ts
@@ -1,10 +1,10 @@
 import * as process from "process";
 
-let result = 1 == 2;
-process.stdout.write("1 equals 2 is " + result);
+let result = <number>1 == 2;
+process.stdout.write("1 equals 2 is " + result + "\n");
 
-result = 1 != 2;
-process.stdout.write("1 not equals 2 is " + result);
+result = 1 != <number>2;
+process.stdout.write("1 not equals 2 is " + result + "\n");
 
 result = 1 > 2;
 process.stdout.write("1 larger than 2 is " + result + "\n");
@@ -16,4 +16,4 @@ result = 1 < 2;
 process.stdout.write("1 less than 2 is " + result + "\n");
 
 result = 1 <= 2;
-process.stdout.write("1 less than or equals 2 is " + result + "\n");
+process.stdout.write("1 less than or equals 2 is " + result);
