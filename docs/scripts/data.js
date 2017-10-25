2d6b348834a6d6af248e1fc6839d9346d3c3236f
Bruno Bernardo <bigsby@live.com>
2017-10-25 22:21:32 +0100

    Start cleaning up data

--- docs/scripts/data.js
+++ docs/scripts/data.js
@@ -1,36 +1,36 @@
-function CreateLanguages(target) {
-    for (var p in target)
-        target[p].languages = [];
-};
+// function CreateLanguages(target) {
+//     for (var p in target)
+//         target[p].languages = [];
+// };
 
 module.exports = function ProcessData(data) {
-    CreateLanguages(data.runtimes);
-    CreateLanguages(data.ides);
+    // CreateLanguages(data.runtimes);
+    // CreateLanguages(data.ides);
 
-    function FindAndReplace(target, prop, list, addLanguage) {
-        var result = [];
-        var source = target[prop];
+    // function FindAndReplace(target, prop, list, addLanguage) {
+    //     var result = [];
+    //     var source = target[prop];
 
-        if (source) {
-            source.forEach(function (id) {
-                var item = list[id];
-                if (item) {
+    //     if (source) {
+    //         source.forEach(function (id) {
+    //             var item = list[id];
+    //             if (item) {
 
-                    if (addLanguage)
-                        item.languages.push(target);
+    //                 if (addLanguage)
+    //                     item.languages.push(target);
 
-                    result.push(item);
-                }
-            });
-            target[prop] = result;
-        }
-    }
+    //                 result.push(item);
+    //             }
+    //         });
+    //         target[prop] = result;
+    //     }
+    // }
 
-    data.languages.forEachValue(function (languageId, language) {
-        FindAndReplace(language, "runtimes", data.runtimes, true);
-        FindAndReplace(language, "ides", data.ides, true);
-        FindAndReplace(language, "tags", data.tags);
-    });
+    // data.languages.forEachValue(function (languageId, language) {
+    //     FindAndReplace(language, "runtimes", data.runtimes, true);
+    //     FindAndReplace(language, "ides", data.ides, true);
+    //     FindAndReplace(language, "tags", data.tags);
+    // });
 
     data.projects.forEachValue(function (projectId, project) {
         project.isImplemented = project.type === 'steps' || (project.languages && project.languages.length);
