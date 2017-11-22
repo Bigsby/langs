e5f854413e60aa209495f24af04ddfe5e6d4de85
Bruno Bernardo <bigsby@live.com>
2017-11-22 13:37:09 +0000

    More Scala Regex

--- src/scala/regex.scala
+++ src/scala/regex.scala
@@ -1,6 +1,13 @@
 object Regex extends App {
     import scala.util.matching.Regex
 
-    if ("123456".matches("^[0-9]+$")) println("yeay")
+    println("Is '^[0-9]+$' mathed by '123456'?")
+    if ("123456".matches("^[0-9]+$")) 
+        println("yes")
+    else
+        println("no")
 
+    println("Groups of '^([a-z]+)\\-(\\d+)$' found in 'abcdef-12345'")
+
+    
 }
